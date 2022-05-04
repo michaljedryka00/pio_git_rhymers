89302b9239bd36aeee70ea13bd0ccd58fa8a854c
Michal Jedryka <235884@edu.p.lodz.pl>
2022-05-04 17:12:21 +0200

    16. Dokumentacja klasy FIFORhymer

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,5 +1,16 @@
 package edu.kis.vh.nursery;
 
+
+/**
+ * klasa FIFORhymer rozszerza klasę DefaultCountingOutRhymer.
+ * klasa posiada atrybut temp będacy instancją klasy DefaultCountingOutRhymer.
+ * element przejmuje atrybuty i metody z rozszerzanej klasy.
+ * metoda countOut jest zmieniona względem rozszerzanej klasy (annotacja @Override)
+ * działanie metody countOut: wczytanie liczb do temp w petli while wykonujacej sie do momentu wyczerpania sie liczb,
+ * zapisanie do zmiennej ret ostatnio wczytanej do temp liczby za pomocą metody countOut z klasy DefaultCountingOutRhymer,
+ * zapisanie pozostałych liczb z powrotem do momentu wyczerpania sie liczb
+ * wartoscia zwracana jest ret - ostatnia wczytana liczba
+ */
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
     private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
